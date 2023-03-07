08f16389692e419e7fdcf74a8fb8b88c58c5bf96
hunterd80 <hunterd777@gmail.com>
2023-03-07 14:55:35 +0900

    80P

---
+++ ch3.py
@@ -0,0 +1,19 @@
+print(1+1)
+print(3-2)
+print(5*2)
+print(6/3)
+
+print(2**3)
+print(10%3)
+print(10//3)
+
+print(10 >3)
+print(4>=7)
+print(10 < 3)
+print(5 <= 5)
+print()
+
+print(3 == 3)
+print(4==2)
+print(3+4 == 7)
+print(1 != 3)
