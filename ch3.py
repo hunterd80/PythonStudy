5ff6c9cb07a41d7b0b645f8ae2c49ce3d5347288
hunterd80 <hunterd777@gmail.com>
2023-03-08 16:44:15 +0900

    3.4 완료

--- ch3.py
+++ ch3.py
@@ -17,3 +17,31 @@ print(3 == 3)
 print(4==2)
 print(3+4 == 7)
 print(1 != 3)
+
+print((3>0) and (3>5))
+print((3>0) or (3>5))
+print(not(1 !=3))
+
+print(2+3*4)
+print((2+3)*4)
+
+number = 2+3*4
+print(number)
+# number = number +2 #2+3*4+2
+# print(number)
+
+number += 2 
+print(number)
+
+number -= 2 
+print(number)
+number *= 2 
+print(number)
+number /= 2 
+print(number)
+number **= 2 
+print(number)
+number //= 2 
+print(number)
+number %= 2 
+print(number)
