5ed3eabf6fe8459c68666bcde7947b96a4ab07ee
hunterd80 <hunterd777@gmail.com>
2023-03-10 18:04:57 +0900

    119P  start

---
+++ ch4.py
@@ -0,0 +1,20 @@
+sentence1 = '나는 소년입니다.'
+print(sentence1, type(sentence1))
+
+sentence2 = '파이썬은 쉬어요'
+print(sentence2, type(sentence2))
+
+sentence3 = """ 나는 소년이고,
+파이썬은 쉬어요."""
+print(sentence3)
+
+jumin = "990229-1234567"
+print("성별 식별번호 : "+ jumin[7])
+
+print("연 :" , jumin[0:2])
+print("월 :" , jumin[2:4])
+print("일 :" , jumin[4:6])
+
+print("생년월일 : "+jumin[:6])
+print("주민등록번호 뒷자리 : "+ jumin[7:])
+print("주민등록번호 뒷자리 : "+ jumin[-7:])
