df1c41ecaa2739392d53305ec4686323bef7841f
hunterd80 <hunterd777@gmail.com>
2023-03-13 15:40:28 +0900

    5.1.3 시작

---
+++ 2week 1day.py
@@ -0,0 +1,19 @@
+subway=[10, 20, 30]
+print(subway)
+
+subway = ["푸", "피글렛", "티거"]
+print(subway)
+
+print(subway.index('피글렛'))
+
+subway.append("이요르")
+print(subway)
+
+subway.insert(1, "루")
+print(subway)
+
+print(subway.pop())      
+print(subway)
+
+subway.clear()
+print(subway)
