15e4ae609d3335903adf6482a8b473712726c919
hunterd80 <hunterd777@gmail.com>
2023-03-25 22:23:55 +0900

    3주  2일차 완료

--- 3week 2day.py
+++ 3week 2day.py
@@ -32,3 +32,15 @@ print("{0: >10}".format(500))
 
 print("{0: >+10}".format(500))
 print("{0: >+10}".format(-500))
+print()
+print("{0:_<10}".format(500))
+
+print("{0:,}".format(100000000000))
+print("{0:+,}".format(100000000000))
+print("{0:+,}".format(-100000000000))
+
+print("{0:^<+30,}".format(100000000000))
+
+print("{0:.2f}".format(5/3))
+
+print("{0:_>+5}".format(100))
