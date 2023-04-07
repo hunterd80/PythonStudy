bbefc2a64e9ea04329d879e463a9be38f0c3bc85
hunterd80 <hunterd777@gmail.com>
2023-04-07 16:16:27 +0900

    4주 4일차 완료

--- 4week 4day.py
+++ 4week 4day.py
@@ -36,5 +36,11 @@ price(5)
 from travel import *
 
 # trip_to = vietnam.VietnamPackage()
-trip_to = thailand.ThailandPackage()
-trip_to.detail()
+# trip_to = thailand.ThailandPackage()
+# trip_to.detail()
+
+import inspect
+import random
+
+print(inspect.getfile(random))
+print(inspect.getfile(thailand))
