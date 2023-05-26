0954e53029a68fbf3d38313027316e21b2d773b7
hunterd80 <hunterd777@gmail.com>
2023-05-26 17:31:44 +0900

    006

---
+++ 100제/006.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://en.wikipedia.org/wiki/Seoul_Metropoltian_Subway"
+resp = requests.get(url)
+html_src =resp.text
+soup = BeautifulSoup(html_src, 'html.parser')
+
+links = soup.find_all("a")
