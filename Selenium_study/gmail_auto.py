c640a0f2c5dc8e871e09b4cb91a38e22dde7359b
hunterd80 <hunterd777@gmail.com>
2023-11-30 04:40:22 +0900

    메일 자동보내기 연습

--- Selenium_study/gmail_auto.py
+++ Selenium_study/gmail_auto.py
@@ -1,6 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 
 import time
 
+
+driver = webdriver.Chrome()
+url = 'https://google.com'
+driver.get(url)
+driver.maximize_window()
+
+time.sleep(1)
+
+action = ActionChains(driver)
+
+
+driver.find_element(By.CSS_SELECTOR,"[class='gb_za gb_jd gb_Ld gb_ie']").click()
+#driver.find_element(By.LINK_TEXT,"로그인").click
+   
+# class="gb_za gb_jd gb_Ld gb_ie" 클래스 명
